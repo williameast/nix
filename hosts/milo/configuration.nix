fca6bf41156802978bb1aa43f47fa5740b6ca830
William East <william.east@mail.mcgill.ca>
2026-03-08 19:15:35 +0100

    fixed config

--- hosts/milo/configuration.nix
+++ hosts/milo/configuration.nix
@@ -24,6 +24,7 @@
     ../../modules/nixos/services/immich.nix
     ../../modules/nixos/services/syncthing.nix
     ../../modules/nixos/services/homepage.nix
+    ../../modules/nixos/services/gitea.nix
     # ../../modules/nixos/services/docker.nix
 
     # I/O devices (printers, monitors, etc.)
